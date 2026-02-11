4da571b9354ee986390b6a8d60dbc469049a0372
Jonas Götz <jonas@42heilbronn.de>
2026-02-11 11:30:31 +0100

    💄 Make footer stick on bottom

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -30,17 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+        <div className="absolute top-0 left-0 right-0 z-[-2] min-h-screen w-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+        <main className="flex-1 flex flex-col">
           {children}
-          <footer className="py-5 sm:py-0 sm:pt-10 text-center text-sm text-muted-foreground mt-5">
-              Made by <strong>Jonas Götz</strong>
-          </footer>
+        </main>
+        <footer className="flex-shrink-0 py-5 sm:py-0 sm:pt-10 text-center text-sm text-muted-foreground mt-5 mb-7">
+          Made by <strong>Jonas Götz</strong>
+        </footer>
 
-          {/* ------------------------------- Bottom Blur ------------------------------ */}
-          <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background" />
-        </div>
+        {/* ------------------------------- Bottom Blur ------------------------------ */}
+        <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background" />
       </body>
     </html>
   );
